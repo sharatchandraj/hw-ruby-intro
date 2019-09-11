415a7831440b3ac90f4e32b1b16c7f7ffbfa11e1
Sharat Chandra <sharat.chandraj@gmail.com>
2019-09-11 22:33:30 +0000

    Added the code to ruby_intro.rb file, tested my progress using spec files. First commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,81 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size < 1
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+    arr=arr.sort()
+    sum = arr[arr.size-1]+arr[arr.size-2]
+    return sum
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? arr,n
+  combos = arr.combination(2).select { |a, b| a + b == n }
+  if combos.empty?
+    return false
+  else
+    return true
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /^[^aeiou\W]/i.match(s) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s.to_i(10)%4==0 and s !~ /[^01]/ and !s.empty?)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(n,p)
+      # assign instance variables
+      if (n.empty? or p <= 0.0)
+        raise ArgumentError
+      end
+      @isbn = n
+      @price = p
+   end
+   
+   def isbn
+     @isbn
+   end
+   
+   def isbn=(isbn)
+     @isbn = isbn
+   end
+   
+   def price
+     @price
+   end
+   
+   def price=(price)
+     @price = price
+   end
+   
+   def price_as_string
+     return "$%0.2f"%[@price]
+   end
 end
